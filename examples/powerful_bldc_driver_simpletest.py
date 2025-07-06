8f2dfa5aa1445dea57b5049480b72c1c33d8736d
Aw3someAndrew <141903453+Aw3someAndrew@users.noreply.github.com>
2025-07-06 21:11:31 +1000

    Delete examples/powerful_bldc_driver_simpletest.py

--- examples/powerful_bldc_driver_simpletest.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
-# SPDX-FileCopyrightText: Copyright (c) 2024 Emily Trau for Steel Bar Robotics
-#
-# SPDX-License-Identifier: Unlicense
